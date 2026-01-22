f24e081641570fff791fb770a5c436bccbfa303e
navarrs <ingrid.navarro.an@gmail.com>
2026-01-22 16:56:58 -0500

    Added safe-st metrics

--- src/scenetokens/utils/metric_utils.py
+++ src/scenetokens/utils/metric_utils.py
@@ -193,7 +193,7 @@ def compute_mutual_information(x: torch.Tensor, y: torch.Tensor, *, normalize: b
     return mutual_information
 
 
-def compute_confusion_matrix(labels: torch.Tensor, predictions: torch.Tensor) -> tuple[torch.Tensor, ...]:
+def compute_binary_confusion_matrix(labels: torch.Tensor, predictions: torch.Tensor) -> tuple[torch.Tensor, ...]:
     """Calculates the confusion matrix between predictions and labels.
 
     Args:
@@ -223,6 +223,40 @@ def compute_confusion_matrix(labels: torch.Tensor, predictions: torch.Tensor) ->
     return true_positives, true_negatives, false_positives, false_negatives
 
 
+def compute_multiclass_accuracy(
+    labels: torch.Tensor, predictions: torch.Tensor, num_classes: int
+) -> tuple[torch.Tensor, ...]:
+    """Computes the precision, recall and F1 scores for multiclass classification.
+
+    Args:
+        labels (torch.Tensor(B, N)): Tensor of target values.
+        predictions (torch.Tensor(B, N)): Tensor of predicted values.
+        num_classes (int): number of classes.
+
+    Returns:
+        precision (torch.Tensor(B)): Accuracy of positive predictions.
+        recall (torch.Tensor(B)): Sensitivity of possitive predictions.
+        f1_score (torch.Tensor(B)): Balance between precision and recall.
+    """
+    assert predictions.shape == labels.shape, "Shapes of predictions and labels must be the same."
+
+    batch_size = labels.shape[0]
+    confusion_matrix = torch.zeros((batch_size, num_classes, num_classes), dtype=torch.float32, device=labels.device)
+    for i in range(batch_size):
+        for target, prediction in zip(labels[i].view(-1), predictions[i].view(-1), strict=False):
+            confusion_matrix[i, target.long(), prediction.long()] += 1
+
+    true_positives = confusion_matrix.diagonal(dim1=1, dim2=2)
+    false_positives = confusion_matrix.sum(dim=1) - true_positives
+    false_negatives = confusion_matrix.sum(dim=2) - true_positives
+
+    precision = true_positives / (true_positives + false_positives + SMALL_EPSILON)
+    recall = true_positives / (true_positives + false_negatives + SMALL_EPSILON)
+    f1_score = 2 * (precision * recall) / (precision + recall + SMALL_EPSILON)
+
+    return precision.mean(dim=1), recall.mean(dim=1), f1_score.mean(dim=1)
+
+
 def compute_accuracy(labels: torch.Tensor, predictions: torch.Tensor) -> tuple[torch.Tensor, ...]:
     """Computes the precision, recall and F1 scores.
 
@@ -235,7 +269,7 @@ def compute_accuracy(labels: torch.Tensor, predictions: torch.Tensor) -> tuple[t
         recall (torch.Tensor(B)): Sensitivity of possitive predictions.
         f1_score (torch.Tensor(B)): Balance between precision and recall.
     """
-    true_positives, _, false_positives, false_negatives = compute_confusion_matrix(labels, predictions)
+    true_positives, _, false_positives, false_negatives = compute_binary_confusion_matrix(labels, predictions)
 
     # Precision
     precision = true_positives / (true_positives + false_positives + SMALL_EPSILON)
